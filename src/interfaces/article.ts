e98fe4914112200b62268207724b1516b644b210
rajesh kumar <mailbox.rajeshkr@gmail.com>
2026-05-10 12:02:39 -0700

    feat(initial): first draft of files and directories

---
+++ src/interfaces/article.ts
@@ -0,0 +1,10 @@
+export default interface Article {
+  documentId: number;
+  title: string;
+  description: string;
+  content: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
