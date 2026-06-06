958e035354e787b92911bc45bf61157a3f0516da
rajesh kumar <mailbox.rajeshkr@gmail.com>
2026-06-06 12:30:32 -0700

    chores: title revise to home page link

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Static consts
-export const SITE_TITLE = 'Personal Blogs';
+export const SITE_TITLE = 'Home';
 export const SITE_DESCRIPTION = 'Welcome to my digital corner. I share unfiltered thoughts, daily musings, and a little bit of everything in between. Join me as I navigate life, explore new hobbies, and share the everyday moments that make life beautiful.';
 
 // Dynamic consts
