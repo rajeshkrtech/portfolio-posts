4e7031bfabeddee30ffe135d896e6f70ea29c9a0
rajesh kumar <mailbox.rajeshkr@gmail.com>
2026-06-06 16:05:00 -0700

    config(astro): use / as base url for homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rajeshkr.com/',
+	site: 'https://rajeshkr.com/blog',
 	base: '/blog',
 	integrations: [mdx(), sitemap(), react()],
 	vite: {
