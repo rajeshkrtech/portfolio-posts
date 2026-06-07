1eea9342754754213c1235e9ac3dfc0dbcad0ed8
rajesh kumar <mailbox.rajeshkr@gmail.com>
2026-06-07 14:41:50 -0700

    config(astro): base url change for blog path

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rajeshkr.com',
-	base: '/',
+	base: '/blog',
 	integrations: [mdx(), sitemap(), react()],
 	vite: {
     	plugins: [tailwindcss()],
